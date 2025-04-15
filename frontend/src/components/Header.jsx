311396a953df1eabb79aa9b21b4cde7e8932eef0
infosanuth <infosanuth@gmail.com>
2025-04-15 09:38:18 +0530

    Create Banner component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div className='flex flex-col md:flex-row bg- bg-[#708090] rounded-lg px-6 md:px-10 lg:px-20'>
+    <div className='flex flex-col md:flex-row bg- bg-[#64748B] rounded-lg px-6 md:px-10 lg:px-20'>
 
       {/* Left Side */}
       <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -11,7 +11,7 @@ const TopDoctors = () => {
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
-      <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors</p>
+      <p className='sm:w-1/3 text-center text-sm relative'>Simply browse through our extensive list of trusted doctors</p>
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'
       style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))' }}>
         {doctors.slice(0,10).map((item,index)=>(
