091dcb7aa8a48d07469669bd8f09e7a5a37d91f0
infosanuth <infosanuth@gmail.com>
2025-04-22 12:16:49 +0530

    Create About us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to Nexus Hospital's Online Appointment Booking System — your trusted gateway to convenient, high-quality healthcare. We make it easier than ever to find the right doctor and book appointments at your convenience, anytime and anywhere.</p>
+          <p>Nexus is dedicated to transforming healthcare through innovative digital solutions. Our platform is designed to streamline appointment management, reduce waiting times, and enhance patient satisfaction. From booking consultations to staying updated on your medical visits, Nexus ensures a smooth and supportive healthcare journey for every user.</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Our vision at Nexus is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4 text-gray-500'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#64748B] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#64748B] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convenience:</b>
+          <p>Access to a network of trusted healthcare professionals in your area.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#64748B] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
+        </div>
+      </div>
+
     </div>
   )
 }
