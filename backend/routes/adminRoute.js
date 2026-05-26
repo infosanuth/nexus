be6022cb55fa8626e82745c6e1345bf068f56409
infosanuth <infosanuth@gmail.com>
2026-05-26 23:01:14 +0530

    feat: implement admin add-speciality route with auth and image upload

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDoctor, allDoctors, loginAdmin, appointmentsAdmin, appointmentCancel, adminDashboard, getMonthlyRevenue, getAppointmentsBySpecialty } from "../controllers/adminController.js";
+import { addDoctor, allDoctors, loginAdmin, appointmentsAdmin, appointmentCancel, adminDashboard, getMonthlyRevenue, getAppointmentsBySpecialty, addSpeciality } from "../controllers/adminController.js";
 import upload from "../middleware/multer.js";
 import authAdmin from "../middleware/authAdmin.js";
 import { changeAvailability } from "../controllers/doctorController.js";
@@ -16,6 +16,7 @@ adminRouter.get('/dashboard', authAdmin, adminDashboard)
 adminRouter.get('/monthly-revenue', authAdmin, getMonthlyRevenue);
 adminRouter.get('/specialty-count',authAdmin, getAppointmentsBySpecialty)
 adminRouter.post('/appointments-doctor',authAdmin,)
+adminRouter.post('/add-speciality', authAdmin, upload.single('image'), addSpeciality)
 
 
 export default adminRouter  
