b1f46e349502210bf5c5e2877e057337e93c7cfd
infosanuth <infosanuth@gmail.com>
2026-06-10 09:17:27 +0530

    feat: add add-session route for doctors

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { doctorList, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard, doctorProfile, updateDoctorProfile } from '../controllers/doctorController.js'
+import { doctorList, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard, doctorProfile, updateDoctorProfile, addSession } from '../controllers/doctorController.js'
 import authDoctor from '../middleware/authDoctor.js'
 
 const doctorRoutre = express.Router()
@@ -11,5 +11,6 @@ doctorRoutre.post('/cancel-appointment', authDoctor, appointmentCancel)
 doctorRoutre.get('/dashboard', authDoctor, doctorDashboard)
 doctorRoutre.get('/profile', authDoctor, doctorProfile)
 doctorRoutre.post('/update-profile', authDoctor, updateDoctorProfile)
+doctorRoutre.post('/add-session', authDoctor, addSession)
 
 export default doctorRoutre
