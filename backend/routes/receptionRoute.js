49a690008a8b006d827a91e37da56e2b49601428
infosanuth <infosanuth@gmail.com>
2026-06-12 08:15:52 +0530

    feat: add reception route for fetching appointments

--- backend/routes/receptionRoute.js
+++ backend/routes/receptionRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { bookWalkInAppointment } from '../controllers/receptionController.js'
+import { bookWalkInAppointment, appointmentsReception } from '../controllers/receptionController.js'
 import authReception from '../middleware/authReception.js'
 
 const receptionRouter = express.Router()
 
 receptionRouter.post('/book-appointment', authReception, bookWalkInAppointment)
+receptionRouter.get('/appointments', authReception, appointmentsReception)
 
 export default receptionRouter
