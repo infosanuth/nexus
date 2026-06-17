bbfdcee33bb04fc3c8b02a56ca9f0142ff902e6e
infosanuth <infosanuth@gmail.com>
2026-06-17 22:06:43 +0530

    feat: add reception route for session creation

--- backend/routes/receptionRoute.js
+++ backend/routes/receptionRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { bookWalkInAppointment, appointmentsReception, sessionsReception } from '../controllers/receptionController.js'
+import { bookWalkInAppointment, appointmentsReception, sessionsReception, addSessionReception } from '../controllers/receptionController.js'
 import authReception from '../middleware/authReception.js'
 
 const receptionRouter = express.Router()
@@ -7,5 +7,6 @@ const receptionRouter = express.Router()
 receptionRouter.post('/book-appointment', authReception, bookWalkInAppointment)
 receptionRouter.get('/appointments', authReception, appointmentsReception)
 receptionRouter.get('/sessions', authReception, sessionsReception)
+receptionRouter.post('/add-session', authReception, addSessionReception)
 
 export default receptionRouter
