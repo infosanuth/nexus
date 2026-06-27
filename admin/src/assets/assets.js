3de318116ec48c0f2c83982f6e9cad4aef03d2a3
infosanuth <infosanuth@gmail.com>
2026-06-27 23:20:28 +0530

    feat: add government hospitals list

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -27,3 +27,27 @@ export const assets = {
     appointments_icon,
     earning_icon
 }
+
+export const governmentHospitals = [
+    "National Hospital of Sri Lanka",
+    "National Hospital Kandy",
+    "National Dental Hospital of Sri Lanka",
+
+    "Teaching Hospital Karapitiya",
+    "Teaching Hospital Peradeniya",
+    "Teaching Hospital Anuradhapura",
+    "Teaching Hospital Batticaloa",
+    "Teaching Hospital Jaffna",
+    "Teaching Hospital Colombo North",
+    "Teaching Hospital Colombo South",
+    "Teaching Hospital Kuliyapitiya",
+    "Teaching Hospital Ratnapura",
+    "Teaching Hospital Mahamodara",
+    "Teaching Hospital Kegalle",
+    "Teaching Hospital Kurunegala",
+
+    "Lady Ridgeway Hospital for Children",
+    "Castle Street Hospital for Women",
+    "De Soysa Hospital for Women",
+    "National Eye Hospital"
+]
---
+++ backend/Hospitals.js
@@ -0,0 +1,23 @@
+export const governmentHospitals = [
+  "National Hospital of Sri Lanka",
+  "National Hospital Kandy",
+  "National Dental Hospital of Sri Lanka",
+
+  "Teaching Hospital Karapitiya",
+  "Teaching Hospital Peradeniya",
+  "Teaching Hospital Anuradhapura",
+  "Teaching Hospital Batticaloa",
+  "Teaching Hospital Jaffna",
+  "Teaching Hospital Colombo North",
+  "Teaching Hospital Colombo South",
+  "Teaching Hospital Kuliyapitiya",
+  "Teaching Hospital Ratnapura",
+  "Teaching Hospital Mahamodara",
+  "Teaching Hospital Kegalle",
+  "Teaching Hospital Kurunegala",
+
+  "Lady Ridgeway Hospital for Children",
+  "Castle Street Hospital for Women",
+  "De Soysa Hospital for Women",
+  "National Eye Hospital"
+];
