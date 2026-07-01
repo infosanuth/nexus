bb5c6ba3034c0c3108a62fb9464fa2f2029d30fb
infosanuth <infosanuth@gmail.com>
2026-07-01 22:36:12 +0530

    feat: integrate multer for doctor profile image upload

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { doctorList, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard, doctorProfile, updateDoctorProfile, addSession, getSessions, deleteSession, getAvailableSessions } from '../controllers/doctorController.js'
 import authDoctor from '../middleware/authDoctor.js'
+import upload from '../middleware/multer.js'
 
 const doctorRoutre = express.Router()
 
@@ -11,7 +12,7 @@ doctorRoutre.post('/complete-appointment', authDoctor, appointmentComplete)
 doctorRoutre.post('/cancel-appointment', authDoctor, appointmentCancel)
 doctorRoutre.get('/dashboard', authDoctor, doctorDashboard)
 doctorRoutre.get('/profile', authDoctor, doctorProfile)
-doctorRoutre.post('/update-profile', authDoctor, updateDoctorProfile)
+doctorRoutre.post('/update-profile', authDoctor, upload.single('image'), updateDoctorProfile)
 doctorRoutre.post('/add-session', authDoctor, addSession)
 doctorRoutre.get('/sessions', authDoctor, getSessions)
 doctorRoutre.post('/delete-session', authDoctor, deleteSession)
